9c29b76da07b3daae0eb3a1540686db69ebb6559
Richard Foster <richard@rpf.me>
2020-12-08 20:48:04 +0000

    Setup storybook

---
+++ src/components/Pin/index.tsx
@@ -0,0 +1,3 @@
+const Pin = () => <span>this is a pin</span>
+
+export default Pin
---
+++ src/components/Pin/stories.tsx
@@ -0,0 +1,8 @@
+import Pin from './'
+
+export default {
+  title: 'Pin',
+  component: Pin
+}
+
+export const Default = () => <Pin />
